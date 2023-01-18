ebedb563aaeeaef0ddfbfa454608214c1475e2e2
lexie70 <alexandracampos658@gmail.com>
2023-01-18 20:58:32 +0000

    importacion useEffect

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,52 @@
-import React from "react";
+import React ,{useState,useEffect} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
+	const [todo,setTodo]= useState("")
+	const [list,setList]= useState([])
+// preventDefault ignora comportamiento de etiquetas predeterminadas
+
+	function submit(e){
+     e.preventDefault()
+	 if (todo === "") {
+		alert("add")
+		
+	 }else{
+		setList([...list, todo]);
+		setTodo("");
+	 }
+	}
+	function equis(num){
+		let newList= list.filter((item,i)=> {if (num !== i ) {
+			return item}
+		})
+		setList (newList)
+	}
+	
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<div>
+			<h1 className="d-flex justify-content-center">todos</h1>
+			<div className="mb-3 tex ">
+				{/*  input */}
+				<input className="form-control " placeholder="What needs to be done?" value={todo}  onChange={(e)=> setTodo (e.target.value)}/>
+			</div>
+			<div className="d-flex justify-content-center">
+				<button type="submit" className="btn btn-primary " onClick={submit}>Submit</button>
+			</div>
+			{/* div contiene funcion map que hace que se muestre la lista del array que el usuario introduce */}
+			<div className="container text-center">
+				{list.map((item,index)=>(
+					<li className="mb-2" key={index}>{item}<span onClick={()=> equis(index)} className="ms-5 bg-secondary p-1 hover">X</span></li>	
+					))}
+			</div>
+		</div>   
 	);
 };
-
+			
 export default Home;
+				
+				
+
+	
