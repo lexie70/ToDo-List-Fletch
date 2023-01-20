174b1aade1af73f065fdf0a465be4418c327d202
lexie70 <alexandracampos658@gmail.com>
2023-01-20 02:49:38 +0000

    prueba de fetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,12 +6,47 @@ import React ,{useState,useEffect} from "react";
 const Home = () => {
 	const [todo,setTodo]= useState("")
 	const [list,setList]= useState([])
-// preventDefault ignora comportamiento de etiquetas predeterminadas
+	
+	// const borrarLista = ()=>{
+	// 	fetch('https://assets.breatheco.de/apis/fake/todos/user/alesanchezr', {
+    //   method: "PUT",
+    //   body: JSON.stringify(todos),
+    //   headers: {
+    //     "Content-Type": "application/json"
+    //   }
+    // })
+    // .then(resp => {
+    //     console.log(resp.ok); // will be true if the response is successfull
+    //     console.log(resp.status); // the status code = 200 or code = 400 etc.
+    //     console.log(resp.text()); // will try return the exact result as string
+    //     return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+    // })
+    // .then(data => {
+    //     //here is were your code should start after the fetch finishes
+    //     console.log(data); //this will print on the console the exact object received from the server
+    // })
+    // .catch(error => {
+    //     //error handling
+    //     console.log(error);
+    // });
+	// };
 
+	const rickAndMorty = ()=>{
+		fetch('https://rickandmortyapi.com/api/character')
+  		.then(response => response.json())
+  		.then(data => console.log(data.results.map((item) => item.name )))
+	}
+
+	useEffect(() =>{
+		rickAndMorty()
+	},[])
+
+	
+	// preventDefault ignora comportamiento de etiquetas predeterminadas
 	function submit(e){
      e.preventDefault()
 	 if (todo === "") {
-		alert("add")
+		alert("add a task")
 		
 	 }else{
 		setList([...list, todo]);
